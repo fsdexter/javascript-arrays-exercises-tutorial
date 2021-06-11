805263c8464600a3fa82769f223f8f29ca9b8889
fsdexter <fsdexter@gmail.com>
2021-06-11 05:57:03 +0000

    solution

---
+++ exercises/14-Divide-and-Conquer!!!/app.js
@@ -0,0 +1,22 @@
+let list_of_numbers = [4, 80, 85, 59, 37,25, 5, 64, 66,	81,20, 64, 41, 22, 76,76, 55, 96, 2, 68];
+
+// your code here
+var mergeTwoList = () => {
+    let oddArr = [];
+    let evenArr = [];
+   
+    for(let i = 0; i < list_of_numbers.length; i++){
+        if( list_of_numbers[i] % 2 == 0){
+            evenArr.push(list_of_numbers[i]);           
+        }
+        else if(list_of_numbers[i] % 2 !== 0){
+            oddArr.push(list_of_numbers[i]);
+        }
+    };
+     let arrAux = [].concat(oddArr,evenArr);
+    return arrAux
+};
+   
+
+
+console.log(mergeTwoList(list_of_numbers))
--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,1 +1,8 @@
 var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+var aux = 0
+for (let i = 0; i < myArray.length; i++){
+    if(myArray[i] > aux){
+        var aux = myArray[i];
+    }
+}
+console.log(aux)
