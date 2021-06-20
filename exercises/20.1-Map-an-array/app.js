2a353b61cdd95536b28703b753a1d4d547a8c352
fsdexter <fsdexter@gmail.com>
2021-06-20 06:06:47 +0000

    solution

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -3,7 +3,10 @@ var arrayOfCelsiusValues = [-2,34,56,-10];
 var arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value){
 	
 	//add your code here and return the new value
-	
+	let celsius = (value * 9/5 ) + 32
+    return celsius
 });
 
 console.log(arrayOfFahrenheitValues);
+
+
